ce4790994f5cb6ef3e030d85815ce60a9fc5fbc4
Oksana Kormilina <keysb@mail.ru>
2024-01-27 18:51:34 +0400

    Add changing timer

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -7,17 +7,17 @@ import Question from "./Question.jsx";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex = userAnswers.length
+  const activeQuestionIndex = userAnswers.length;
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(
-    function handleSelectAnswer(selectedAnswer) {
-      setUserAnswers((prevUserAnswers) => {
-        return [...prevUserAnswers, selectedAnswer];
-      });
-    },
-    []
-  );
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  },
+  []);
 
   if (quizIsComplete) {
     return (
