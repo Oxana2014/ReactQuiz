530cd81050f54c77038052187a6e40db8e51e86c
Oksana Kormilina <keysb@mail.ru>
2024-01-27 12:32:29 +0400

    Refactor. Move logic form Quiz component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,27 +1,51 @@
+import { useState } from "react";
+
 import Answers from "./Answers";
 import QuestionTimer from "./QuestionTimer";
+import QUESTIONS from "../questions.js"
 
 export default function Question({
-  questionText,
-  answers,
+    index,
   onSelectAnswer,
-  selectedAnswer,
-  answerState,
-  onSkipAnswer
+  onSkipAnswer,
 }) {
+  const [answer, setAnswer] = useState({
+    selectedAnswer: "",
+    isCorrect: null,
+  });
+
+  function handleSelectAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+    setTimeout(() => {
+        setAnswer({
+            selectedAnswer: answer,
+            isCorrect: QUESTIONS[index].answers[0] === answer,
+          });
+    }, 1000)
+    setTimeout(() => {
+       onSelectAnswer(answer) 
+    }, 2000);
+  }
+  let answerState = ''
+
+  if(answer.selectedAnswer && answer.isCorrect === null) {
+    answerState = answer.isCorrect ? 'correct' : 'wrong'
+  } else if(answer.selectedAnswer) {
+answerState = 'answered'
+  }
+
   return (
     <div id="question">
-      <QuestionTimer
-        timeout={10000}
-        onTimeout={onSkipAnswer}
-      />
-      <h2>{questionText}</h2>
+      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <h2>{QUESTIONS[index].text}</h2>
       <Answers
-      
-        answers={answers}
-        selectedAnswer={selectedAnswer}
+        answers={QUESTIONS[index].answers}
+        selectedAnswer={answer.selectedAnswer}
         answerState={answerState}
-        onSelect={onSelectAnswer}
+        onSelect={handleSelectAnswer}
       />
     </div>
   );
